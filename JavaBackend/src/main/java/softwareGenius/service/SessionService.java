1fdbc53b44b183f272740f26b0b0e0e7f5a77bd7
Ethan <ganx0005@e.ntu.edu.sg>
2020-03-18 21:40:15 +0800

    sessin

--- JavaBackend/src/main/java/softwareGenius/service/SessionService.java
+++ JavaBackend/src/main/java/softwareGenius/service/SessionService.java
@@ -1,7 +1,25 @@
 package softwareGenius.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import softwareGenius.mapper.SessionDao;
+import softwareGenius.model.Session;
 
+import java.util.List;
 @Service
 public class SessionService {
+
+    @Autowired
+    private SessionDao sessionDao;
+
+    public Booolean addSession(Integer sessionId, Integer userId, LocalDateTime loginTime){
+        return sessionDao.addSession(sessionId, userId, loginTime);
+    }
+    public Boolean updateSessionEndTime(Integer sessionId, LocalDateTime logoutTime){
+        return sessionDao.updateSessionEndTime(sessionId, logoutTime);
+    }
+    public List<Session> getSessionByUserID(Integer sessionId){
+        return sessionDao.getSessionByUserID(sessionId);
+    }
 }
