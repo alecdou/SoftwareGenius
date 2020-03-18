fc7dbb4c95c1fcf5f77d1585b1e2284e531ac606
Ethan <ganx0005@e.ntu.edu.sg>
2020-03-18 21:00:31 +0800

    question

--- JavaBackend/src/main/java/softwareGenius/mapper/QuestionDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/QuestionDao.java
@@ -1,4 +1,11 @@
 package softwareGenius.mapper;
-
+import softwareGenius.model.Question
 public interface QuestionDao {
+
+    Boolean addNewQuestion(question Question)
+    Boolean updateQuestion(question Question)
+    Boolean deleteQuestion(@Param("questionId") Integer questionId)
+    Question[*] getQuestionsByCategory(@Param("category") String category, @Param("difficultyLevel") Integer difficultyLevel,@Param("limit") Integer limit)
 }
+
+//@Param("questionId") Integer questionId,@Param("category") String category, @Param("problem") String problem, @Param("choice1") String choice1, @Param("choice2") String choice2, @Param("choice3") String choice3, @Param("choice4") String choice4,  @Param("answer") Integer answer, @Param("difficultyLevel") Integer difficultyLevel
