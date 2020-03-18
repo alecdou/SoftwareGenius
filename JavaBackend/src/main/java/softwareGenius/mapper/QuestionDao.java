6c60f05f94d400124ad29887d8cf6113b748ac90
Ethan <ganx0005@e.ntu.edu.sg>
2020-03-18 21:46:28 +0800

    debug

--- JavaBackend/src/main/java/softwareGenius/mapper/QuestionDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/QuestionDao.java
@@ -1,11 +1,12 @@
 package softwareGenius.mapper;
 import softwareGenius.model.Question;
+import java.util.List;
 public interface QuestionDao {
 
-    Boolean addNewQuestion(question Question);
-    Boolean updateQuestion(question Question);
+    Boolean addNewQuestion(Question question);
+    Boolean updateQuestion(Question question);
     Boolean deleteQuestion(@Param("questionId") Integer questionId);
-    Question[*] getQuestionsByCategory(@Param("category") String category, @Param("difficultyLevel") Integer difficultyLevel,@Param("limit") Integer limit);
+    List<Question> getQuestionsByCategory(@Param("category") String category, @Param("difficultyLevel") Integer difficultyLevel,@Param("limit") Integer limit);
 }
 
 //@Param("questionId") Integer questionId,@Param("category") String category, @Param("problem") String problem, @Param("choice1") String choice1, @Param("choice2") String choice2, @Param("choice3") String choice3, @Param("choice4") String choice4,  @Param("answer") Integer answer, @Param("difficultyLevel") Integer difficultyLevel
--- JavaBackend/src/main/java/softwareGenius/mapper/SessionDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/SessionDao.java
@@ -1,7 +1,8 @@
 package softwareGenius.mapper;
 import softwareGenius.model.Session
+import java.util.List;
 public interface SessionDao {
     Booolean addSession(@Param("sessionId") Integer sessionId, @Param("userId") Integer userId, @Param("loginTime") LocalDateTime loginTime);
     Boolean updateSessionEndTime(@Param("sessionId") Integer sessionId, @Param("logoutTime") LocalDateTime logoutTime);
-    Session[*] getSessionByUserID(@Param("sessionId") Integer sessionId);
+    List<Session> getSessionByUserID(@Param("sessionId") Integer sessionId);
 }
