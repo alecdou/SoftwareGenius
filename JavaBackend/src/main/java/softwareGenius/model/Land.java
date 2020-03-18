ba73732cfbdb6bb94ab3823aff9f8a153d503a2c
Kevin <jinc0008@e.ntu.edu.sg>
2020-03-18 11:06:48 +0800

    Auto stash before merge of "master" and "origin/master"

--- JavaBackend/src/main/java/softwareGenius/model/Land.java
+++ JavaBackend/src/main/java/softwareGenius/model/Land.java
@@ -1,4 +1,37 @@
 package softwareGenius.model;
 
 public class Land {
+    private Integer landId;
+    private Integer ownerId;
+    private Integer ownerDifficulty;
+
+    public Land(Integer landId, Integer ownerId, Integer ownerDifficulty) {
+        this.landId = landId;
+        this.ownerId = ownerId;
+        this.ownerDifficulty = ownerDifficulty;
+    }
+
+    public Integer getLandId() {
+        return landId;
+    }
+
+    public void setLandId(Integer landId) {
+        this.landId = landId;
+    }
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Integer ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Integer getOwnerDifficulty() {
+        return ownerDifficulty;
+    }
+
+    public void setOwnerDifficulty(Integer ownerDifficulty) {
+        this.ownerDifficulty = ownerDifficulty;
+    }
 }
--- JavaBackend/src/main/java/softwareGenius/model/NPC.java
+++ JavaBackend/src/main/java/softwareGenius/model/NPC.java
@@ -1,4 +1,37 @@
 package softwareGenius.model;
 
 public class NPC {
+    private Integer npcId;
+    private Integer difficulty;
+    private Integer hitPoint;
+
+    public NPC(Integer npcId, Integer difficulty, Integer hitPoint) {
+        this.npcId = npcId;
+        this.difficulty = difficulty;
+        this.hitPoint = hitPoint;
+    }
+
+    public Integer getNpcId() {
+        return npcId;
+    }
+
+    public void setNpcId(Integer npcId) {
+        this.npcId = npcId;
+    }
+
+    public Integer getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Integer difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Integer getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(Integer hitPoint) {
+        this.hitPoint = hitPoint;
+    }
 }
