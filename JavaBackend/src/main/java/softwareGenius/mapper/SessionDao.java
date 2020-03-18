98ffa9fd6d77fde0fd7e04628a107d03d5306f26
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2020-03-18 22:57:23 +0800

    Fixed syntax error for session Dao

--- JavaBackend/src/main/java/softwareGenius/mapper/SessionDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/SessionDao.java
@@ -1,9 +1,11 @@
 package softwareGenius.mapper;
-import softwareGenius.model.Session
+import softwareGenius.model.Session;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
 public interface SessionDao {
-    Booolean addSession(@Param("sessionId") Integer sessionId, @Param("userId") Integer userId, @Param("loginTime") LocalDateTime loginTime);
+    Boolean addSession(@Param("sessionId") Integer sessionId, @Param("userId") Integer userId, @Param("loginTime") LocalDateTime loginTime);
     Boolean updateSessionEndTime(@Param("sessionId") Integer sessionId, @Param("logoutTime") LocalDateTime logoutTime);
     List<Session> getSessionByUserID(@Param("sessionId") Integer sessionId);
 }
--- JavaBackend/src/main/java/softwareGenius/service/LeaderboardService.java
+++ JavaBackend/src/main/java/softwareGenius/service/LeaderboardService.java
@@ -36,15 +36,15 @@ public class LeaderboardService {
         return all;
     }
 
-    @Scheduled(cron="*/30 * * * *")
+    @Scheduled(cron="* * * * *")
     // Update evey 30 minutes
     public void cronUpdate(){
         try{
-            leaderboardDao.setTopUserForWorld1(leaderboardDao.getLeaderBoardByWorldName("1", 0, 1000));
-            leaderboardDao.setTopUserForWorld2(leaderboardDao.getLeaderBoardByWorldName("2", 0, 1000));
-            leaderboardDao.setTopUserForWorld3(leaderboardDao.getLeaderBoardByWorldName("3", 0, 1000));
-            leaderboardDao.setTopUserForWorld4(leaderboardDao.getLeaderBoardByWorldName("4", 0, 1000));
-            leaderboardDao.setTopUserForGeneral(leaderboardDao.getGeneralLeaderBoard(0, 1000));
+//            leaderboardDao.setTopUserForWorld1(leaderboardDao.getLeaderBoardByWorldName("1", 0, 1000));
+//            leaderboardDao.setTopUserForWorld2(leaderboardDao.getLeaderBoardByWorldName("2", 0, 1000));
+//            leaderboardDao.setTopUserForWorld3(leaderboardDao.getLeaderBoardByWorldName("3", 0, 1000));
+//            leaderboardDao.setTopUserForWorld4(leaderboardDao.getLeaderBoardByWorldName("4", 0, 1000));
+//            leaderboardDao.setTopUserForGeneral(leaderboardDao.getGeneralLeaderBoard(0, 1000));
             System.out.println("Leader Board Updated!");
         } catch (Exception e){
             e.printStackTrace();
