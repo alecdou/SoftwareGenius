74c2337cf2e261d9266b6c2e1d725a3cc843e7f8
Kevin <jinc0008@e.ntu.edu.sg>
2020-03-18 15:06:35 +0800

    land&npc dao

--- JavaBackend/src/main/java/softwareGenius/mapper/NPCDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/NPCDao.java
@@ -1,4 +1,26 @@
 package softwareGenius.mapper;
 
+import softwareGenius.model.NPC;
+
 public interface NPCDao {
+    /**
+     * add an NPC to NPCTable
+     * @param npc
+     * @return
+     */
+    Boolean addNPC(NPC npc);
+
+    /**
+     * delete an NPC from NPCTable
+     * @param npcId
+     * @return
+     */
+    Boolean deleteNPC(Integer npcId);
+
+    /**
+     * get a random NPC by a difficulty level from NPCTable
+     * @param difficultyLevel
+     * @return NPC object
+     */
+    NPC getNPCByDifficultyLevel(Integer difficultyLevel);
 }
