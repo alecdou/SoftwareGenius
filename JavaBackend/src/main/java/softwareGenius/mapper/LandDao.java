6a41c0dbbe84990881d87ebfd80ec01b8633a082
Kevin <jinc0008@e.ntu.edu.sg>
2020-03-18 19:21:16 +0800

    npc dao

--- JavaBackend/src/main/java/softwareGenius/mapper/LandDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/LandDao.java
@@ -1,34 +1,48 @@
 package softwareGenius.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 import softwareGenius.model.Land;
 import java.util.List;
 
+@Mapper
+@Component
 public interface LandDao {
     /**
      * add a land in a world to landTable
-     * @param land
-     * @return
+     * @param land land object
+     * @return success or not
      */
     Boolean addLand(Land land);
 
     /**
      * delete a land from landTable
-     * @param landId
-     * @return
+     * @param landId the id of the land
+     * @return success or not
      */
     Boolean deleteLand(Integer landId);
 
     /**
      * get all lands from a world
-     * @param worldId
+     * @param worldId the id of the world
      * @return a list of lands
      */
     List<Land> getLandByWorldId(Integer worldId);
 
     /**
      * get a land information by its id
-     * @param landId
+     * @param landId the id of the land
      * @return land object
      */
     Land getLandByLandId(Integer landId);
+
+    /**
+     * change the owner and ownerDifficultyLevel of a land
+     * @param landId the id of the land
+     * @param newOwnerId the id of the new owner
+     * @param newDifficultyLevel the new difficulty level
+     * @return success or not
+     */
+    Boolean changeOwner(@Param("lid") Integer landId, @Param("nid") Integer newOwnerId, @Param("ndl") Integer newDifficultyLevel);
 }
--- JavaBackend/src/main/java/softwareGenius/mapper/NPCDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/NPCDao.java
@@ -1,25 +1,29 @@
 package softwareGenius.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 import softwareGenius.model.NPC;
 
+@Mapper
+@Component
 public interface NPCDao {
     /**
      * add an NPC to NPCTable
-     * @param npc
-     * @return
+     * @param npc npc
+     * @return success or not
      */
     Boolean addNPC(NPC npc);
 
     /**
      * delete an NPC from NPCTable
-     * @param npcId
-     * @return
+     * @param npcId the id of the npc
+     * @return success or not
      */
     Boolean deleteNPC(Integer npcId);
 
     /**
      * get a random NPC by a difficulty level from NPCTable
-     * @param difficultyLevel
+     * @param difficultyLevel level of difficulty
      * @return NPC object
      */
     NPC getNPCByDifficultyLevel(Integer difficultyLevel);
