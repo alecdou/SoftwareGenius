d57717838ecb27575343be841a32f40ca80f9539
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2020-03-18 13:48:16 +0800

    Added Java doc

--- JavaBackend/src/main/java/softwareGenius/mapper/ErrorQuestionDao.java
+++ JavaBackend/src/main/java/softwareGenius/mapper/ErrorQuestionDao.java
@@ -1,9 +1,26 @@
 package softwareGenius.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import softwareGenius.model.ErrorQuestion;
 
+/**
+ * Used to record and get questions that were wrongly answered by the user
+ */
+@Mapper
 public interface ErrorQuestionDao {
+    /**
+     * Record questions that were wrongly answered by a student
+     * @param userId id of the student
+     * @param questionId id of the question
+     * @param wrongAnswer wrong answer by the student
+     */
     void addNewErrorQuestion(@Param("userId") Integer userId, @Param("questionId") Integer questionId, @Param("wrongAnswer") String wrongAnswer);
+
+    /**
+     * Get questions that were wrongly answered by a student
+     * @param userId id of the student
+     * @return a list of ErrorQuestion objects
+     */
     ErrorQuestion[] getErrorQuestionByUserId(@Param("userId") Integer userId);
 }
