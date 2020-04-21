de361e607a72c748b8a3305121ce3d6a726d8fa7
Kevin <jinc0008@e.ntu.edu.sg>
2020-04-21 15:39:50 +0800

    it works now

--- JavaBackend/src/main/java/softwareGenius/model/NPC.java
+++ JavaBackend/src/main/java/softwareGenius/model/NPC.java
@@ -1,51 +1,51 @@
-//package softwareGenius.model;
-//
-//public class NPC {
-//    private Integer npcId;
-//    private Integer difficultyLevel;
-//    private Integer hitPoint;
-//    private Integer attackPoint;
-//
-//    public NPC() {
-//
-//    }
-//
-//    public NPC(Integer npcId, Integer difficultyLevel, Integer hitPoint, Integer attackPoint) {
-//        this.npcId = npcId;
-//        this.difficultyLevel = difficultyLevel;
-//        this.hitPoint = hitPoint;
-//        this.attackPoint = attackPoint;
-//    }
-//
-//    public Integer getAttackPoint() {
-//        return attackPoint;
-//    }
-//
-//    public void setAttackPoint(Integer attackPoint) {
-//        this.attackPoint = attackPoint;
-//    }
-//
-//    public Integer getDifficultyLevel() {
-//        return difficultyLevel;
-//    }
-//
-//    public void setDifficultyLevel(Integer difficultyLevel) {
-//        this.difficultyLevel = difficultyLevel;
-//    }
-//
-//    public Integer getNpcId() {
-//        return npcId;
-//    }
-//
-//    public void setNpcId(Integer npcId) {
-//        this.npcId = npcId;
-//    }
-//
-//    public Integer getHitPoint() {
-//        return hitPoint;
-//    }
-//
-//    public void setHitPoint(Integer hitPoint) {
-//        this.hitPoint = hitPoint;
-//    }
-//}
+package softwareGenius.model;
+
+public class NPC {
+    private Integer npcId;
+    private Integer difficultyLevel;
+    private Integer hitPoint;
+    private Integer attackPoint;
+
+    public NPC() {
+
+    }
+
+    public NPC(Integer npcId, Integer difficultyLevel, Integer hitPoint, Integer attackPoint) {
+        this.npcId = npcId;
+        this.difficultyLevel = difficultyLevel;
+        this.hitPoint = hitPoint;
+        this.attackPoint = attackPoint;
+    }
+
+    public Integer getAttackPoint() {
+        return attackPoint;
+    }
+
+    public void setAttackPoint(Integer attackPoint) {
+        this.attackPoint = attackPoint;
+    }
+
+    public Integer getDifficultyLevel() {
+        return difficultyLevel;
+    }
+
+    public void setDifficultyLevel(Integer difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public Integer getNpcId() {
+        return npcId;
+    }
+
+    public void setNpcId(Integer npcId) {
+        this.npcId = npcId;
+    }
+
+    public Integer getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(Integer hitPoint) {
+        this.hitPoint = hitPoint;
+    }
+}
--- JavaBackend/src/main/java/softwareGenius/service/NPCService.java
+++ JavaBackend/src/main/java/softwareGenius/service/NPCService.java
@@ -1,25 +1,25 @@
-//package softwareGenius.service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import softwareGenius.mapper.NPCDao;
-//import softwareGenius.model.NPC;
-//
-//@Service
-//public class NPCService {
-//    private NPCDao npcDao;
-//
-//    @Autowired
-//    public NPCService(NPCDao npcDao) {
-//        this.npcDao = npcDao;
-//    }
-//
-//    /**
-//     * get an NPC by the difficulty level
-//     * @param difficultyLevel the difficulty level of NPC
-//     * @return NPC object
-//     */
-//    public NPC getNPCByDifficultyLevel(Integer difficultyLevel) {
-//        return npcDao.getNPCByDifficultyLevel(difficultyLevel);
-//    }
-//}
+package softwareGenius.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import softwareGenius.mapper.NPCDao;
+import softwareGenius.model.NPC;
+
+@Service
+public class NPCService {
+    private NPCDao npcDao;
+
+    @Autowired
+    public NPCService(NPCDao npcDao) {
+        this.npcDao = npcDao;
+    }
+
+    /**
+     * get an NPC by the difficulty level
+     * @param difficultyLevel the difficulty level of NPC
+     * @return NPC object
+     */
+    public NPC getNPCByDifficultyLevel(Integer difficultyLevel) {
+        return npcDao.getNPCByDifficultyLevel(difficultyLevel);
+    }
+}
